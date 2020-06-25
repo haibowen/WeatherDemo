ca6afae19c30856f8dceb5e061c22a8ccc95d4dc
wenhaibo <1461154748@qq.com>
2020-06-25 20:11:45 +0800

    加入fragment

--- app/src/main/java/com/example/weatherdemo/ui/place/PlaceFragment.kt
+++ app/src/main/java/com/example/weatherdemo/ui/place/PlaceFragment.kt
@@ -4,11 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.weatherdemo.R
 import kotlinx.android.synthetic.main.fragment_place.*
 
 class PlaceFragment : Fragment() {
@@ -20,6 +23,7 @@ class PlaceFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        //return  inflater.inflate(R.layout.fragment_place,container,false)
         return  inflater.inflate(R.layout.fragment_place,container,false)
 
     }
@@ -42,7 +46,19 @@ class PlaceFragment : Fragment() {
 
             }
         }
-        viewModel
+        viewModel.placeLiveData.observe(this, Observer { result->
+            val  places=result.getOrNull()
+            if (places!=null){
+                recyclerView.visibility=View.VISIBLE
+                bgImageView.visibility=View.GONE
+                viewModel.placeList.clear()
+                viewModel.placeList.addAll(places)
+                adapter.notifyDataSetChanged()
+            }else{
+                Toast.makeText(activity,"未能查询到任何地点",Toast.LENGTH_SHORT).show()
+                result.exceptionOrNull()?.printStackTrace()
+            }
+        })
     }
 
 
