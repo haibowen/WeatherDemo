6a94768beb75051b53eb09d21cc8149743d96bda
hyplo <hyplo@MacBook-Pro.local>
2020-06-26 14:52:43 +0800

    加入天气界面布局

---
+++ app/src/main/java/com/example/weatherdemo/ui/weather/WeatherActivity.kt
@@ -0,0 +1,12 @@
+package com.example.weatherdemo.ui.weather
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.weatherdemo.R
+
+class WeatherActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_weather)
+    }
+}
